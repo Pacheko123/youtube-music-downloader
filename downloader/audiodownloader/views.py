971cd6f0bbfde9155127629f167f1262f7de02b6
Pacheko123 <oluochp6@gmail.com>
2021-11-23 23:23:17 +0300

    impliment youtube keyword search

--- downloader/audiodownloader/views.py
+++ downloader/audiodownloader/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from pafy.pafy import new
+from download.download import Download
+from search.search import GoogleSearch
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
 def result(request,keyword):
+    newsearch = GoogleSearch()
+    url = newsearch.search(keyword)
+
     
+    downloadVideo= Download()
+    downloadVideo.getDetails(keyword)
     return render(request,index)
--- src/search/search.py
+++ src/search/search.py
@@ -1,10 +1,11 @@
 
 from googleapiclient.discovery import build
 from pprint import PrettyPrinter
+import pafy
 
 class GoogleSearch:
     def search(keyword):
-        api_key="AIzaSyC38udAOc03YcEXO41SwnqxPJEjpaxudio"
+        api_key=""
 
         youtube = build('youtube','v3',developerKey = api_key)
         print(type(youtube))
@@ -15,8 +16,13 @@ class GoogleSearch:
         for item in res['items']:
             videoId=item['snippet']['id.videoId']
             videoUrl = "https://www.youtube.com/watch?v="+videoId 
-            return videoUrl
+            
+            # url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
+            video = pafy.new(videoUrl)
+            file_size = video.get_filesize()
 
-        pp = PrettyPrinter()
-        pp.pprint(res)
+            '''Get the best audio file for download'''
+            bestaudiodownload = video.getbestaudio()
+            return video.title,videoUrl,video.author,video.duration,video.viewcount,video.likes,bestaudiodownload
+            
         
