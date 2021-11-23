4d3d53d42ccc094191e89dde42e7f93613a9fc66
Pacheko123 <oluochp6@gmail.com>
2021-11-23 13:24:37 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  corePlugins: {
+    // ...
+   container: false,
+  },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        defaultTheme:'#A5AB0A',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
