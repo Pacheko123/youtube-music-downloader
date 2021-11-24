104f216b08e94f32bac8bb6950dff8228e39e729
Pacheko123 <oluochp6@gmail.com>
2021-11-24 14:41:33 +0300

    added the functionalities ui in index.html

--- downloader/audiodownloader/views.py
+++ downloader/audiodownloader/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render
-from pafy.pafy import new
-from download.download import Download
-from search.search import GoogleSearch
+from googleapiclient.discovery import build
+from pprint import PrettyPrinter
+import pafy
+# from download.download import Download
+# from search.search import GoogleSearch
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
 def result(request,keyword):
-    newsearch = GoogleSearch()
-    url = newsearch.search(keyword)
+        api_key=""
 
-    
-    downloadVideo= Download()
-    downloadVideo.getDetails(keyword)
-    return render(request,index)
+        youtube = build('youtube','v3',developerKey = api_key)
+        print(type(youtube))
+
+        request = youtube.search().list(q=keyword,part='snippet',type='video')
+        print(type(request))
+        res = request.execute()
+        for item in res['items']:
+            videoId=item['snippet']['id.videoId']
+            videoUrl = "https://www.youtube.com/watch?v="+videoId 
+            
+            # url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
+            video = pafy.new(videoUrl)
+            file_size = video.get_filesize()
+
+            '''Get the best audio file for download'''
+            bestaudiodownload = video.getbestaudio()
+            print(type(video))
+
+        # return video.title,videoUrl,video.author,video.duration,video.viewcount,video.likes,bestaudiodownload
+        return render(request,index,video)
+
+def download(request):
+    pass
+            
--- downloader/downloader/settings.py
+++ downloader/downloader/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b7m7klf(v@_ty079u7xm8n0*^x)^01(+x2ss#28d2^4aw$_1%w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.108','127.0.0.1']
 
 
 # Application definition
